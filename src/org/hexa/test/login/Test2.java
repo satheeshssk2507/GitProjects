93cd03f7d916d4657b921fe4e3c77372006a2e64
ADMIN <ADMIN@192.168.1.41>
2023-12-02 17:00:24 +0530

    added additional changes...

--- src/org/hexa/test/login/Test2.java
+++ src/org/hexa/test/login/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		for (int i = 0; i <=5; i++) {
 			for (int j = 5; j>=i; j--) {
-				System.out.print("*");
+				System.out.print("10");
 			}
 			System.out.println();
 		}
